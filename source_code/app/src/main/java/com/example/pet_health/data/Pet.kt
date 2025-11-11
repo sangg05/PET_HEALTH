c975b8a51aef6b63362692c6c9ad7f4d97c25cad
Tuyet Sang <sanghtt1001@ut.edu.vn>
2025-11-11 18:08:38 +0700

    folder Strcture

--- source_code/app/src/main/java/com/example/pet_health/data/Pet.kt
+++
@@ -1,8 +0,0 @@
-@Entity(tableName = "pets")
-data class Pet(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val name: String,
-    val type: String,
-    val age: Int,
-    val weight: Double
-)
--- source_code/app/src/main/java/com/example/pet_health/repository/PetRepository.kt
+++ source_code/app/src/main/java/com/example/pet_health/repository/PetRepository.kt
@@ -1,4 +1,4 @@
-class PetRepository(private val petDao: PetDao) {
-    suspend fun addPet(pet: Pet) = petDao.insertPet(pet)
-    suspend fun getAllPets() = petDao.getAllPets()
-}
+//class PetRepository(private val petDao: PetDao) {
+//    suspend fun addPet(pet: Pet) = petDao.insertPet(pet)
+//    suspend fun getAllPets() = petDao.getAllPets()
+//}
